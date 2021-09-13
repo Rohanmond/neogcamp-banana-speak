13c790eda4868c6d9477ffd2cafebf29afeca3a1
Rohan Mondal <mondalrohan108@gmail.com>
2021-09-13 13:09:35 +0530

    updated index and js

--- app.js
+++ app.js
@@ -8,9 +8,27 @@
 var buttonTranslate=document.querySelector("#btn-translate")
 var txtInput=document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output");
+var serverURL="https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text){
+    return serverURL+"?text="+text;
+}
+function errorHandler(error){
+    console.log("Error occured:",error);
+    alert("something wrong with server.Try again after some time!")
+}
+
 function clickHandler(){
-    outputDiv.innerText=txtInput.value+" jdhgkh ";
+    fetch(getTranslationURL(txtInput.value))
+    .then(response=>response.json())
+    .then(json=>{
+        var translatedText=json.contents.translated;
+        outputDiv.innerText=translatedText;
+    })
+    .catch(errorHandler);
 }
 
 buttonTranslate.addEventListener("click",clickHandler);
+
+
 
